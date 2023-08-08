40c3019b2fd20704857f182de8930045cf20b158
SimeonGachev <simeon@alphabite.io>
2023-08-09 01:27:01 +0300

    new structure with dependecy inject

---
+++ src/newVersion/competition/competition.js
@@ -0,0 +1,40 @@
+export default function buildMakeCompetition({ Id }) {
+  return async function makeCompetition({
+    id = Id.newId(),
+    organiser,
+    name,
+    createdOn = Date.now(),
+    modifiedOn = Date.now(),
+    participants = [],
+    open = true,
+    scores = {},
+    ranking = [],
+  } = {}) {
+    if (!Id.isValidId(id)) {
+      throw new Error("invalid Id");
+    }
+
+    if (!organiser) {
+      throw new Error("competition needs an organiser");
+    }
+
+    if (!name) {
+      throw new Error("competition needs a name");
+    }
+
+    return Object.freeze({
+      getId: () => id,
+      getOrganiser: () => organiser,
+      getName: () => name,
+      getCreatedOn: () => createdOn,
+      getModifiedOn: () => modifiedOn,
+      getPartitipants: () => participants,
+      getScores: () => scores,
+      getRanking: () => ranking,
+      isOpen: () => open,
+      close: () => {
+        open = false;
+      },
+    });
+  };
+}
---
+++ src/newVersion/competition/index.js
@@ -0,0 +1,6 @@
+import buildMakeCompetition from "./competition.js";
+import Id from "../Id/index.js";
+
+const makeCompetition = buildMakeCompetition({ Id });
+
+export default makeCompetition;
